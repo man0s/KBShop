f361100542ac43715ee55bddbed1f4d4000126c0
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2019-10-11 22:29:21 +0300

    FIX product text overflow

--- FE/src/eshop/services/product.service.ts
+++ FE/src/eshop/services/product.service.ts
@@ -6,7 +6,8 @@ import { Product } from '../shared/product.model';
 })
 export class ProductService {
 
-  products = [ new Product('Apple iPhone 11 Pro 256GB', 'https://i.imgur.com/fKEuHYq.jpg', 1429, 1)
+  products = [ new Product('Apple iPhone 11 Pro 256GB', 'https://i.imgur.com/fKEuHYq.jpg', 1429, 1),
+    new Product('Samsung QLED 4K TV QE55Q70R 55" 4Κ Ultra HD', 'https://i.imgur.com/U4nbPb0.jpg', )
   ];
 
   constructor() { }
