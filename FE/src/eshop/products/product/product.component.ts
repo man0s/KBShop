0ba04510f8a9da603668415add997d742582502d
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2019-10-11 20:28:30 +0300

    FIX product component css bug

--- FE/src/eshop/products/product/product.component.ts
+++ FE/src/eshop/products/product/product.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-product',
+  // tslint:disable-next-line:component-selector
+  selector: '[app-product]',
   templateUrl: './product.component.html',
   styleUrls: ['./product.component.css']
 })
