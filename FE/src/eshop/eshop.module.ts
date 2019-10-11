048ab98fbd57209c1a8c515ca4878b62a212a430
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2019-10-11 19:57:41 +0300

    ADD navbar, products & product init components

--- FE/src/eshop/eshop.module.ts
+++ FE/src/eshop/eshop.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { EShopComponent } from './eshop.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductComponent } from './products/product/product.component';
 
 @NgModule({
   declarations: [
-    EShopComponent
+    EShopComponent,
+    NavbarComponent,
+    ProductsComponent,
+    ProductComponent
   ],
   imports: [
     BrowserModule
