c39d20a6619bb43a07d5a8ef240d78809b91fa80
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2019-10-13 16:29:11 +0300

    FIX http response pipe

--- FE/src/eshop/services/product.service.ts
+++ FE/src/eshop/services/product.service.ts
@@ -3,27 +3,22 @@ import { Product } from '../models/product.model';
 import { Observable } from 'rxjs';
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
-import { CachingServiceBase } from "./caching.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService extends CachingServiceBase {
+export class ProductService{
 
   private products: Observable<Product[]>;
 
   constructor(private http: HttpClient) {
-    super();
   }
 
 
   public all(): Observable<Product[]> {
-    return this.cache<Product[]>(() => this.products,
-      (val: Observable<Product[]>) => this.products = val,
-      () => this.http
-        .get("./assets/products.json").pipe(
-          map((response: any) => response)
-        ));
+    return this.http.get("./assets/products.json")
+      .pipe(map((response: any) => response)
+      );
   }
 
 
