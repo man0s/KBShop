59e5301b64801cd73fcf9b1c88905c2044f569dd
Emmanouil Katefidis <katefidis@ceid.upatras.gr>
2019-10-14 11:48:08 +0300

    ADD new navbar dropdown

--- FE/src/eshop/services/product.service.ts
+++ FE/src/eshop/services/product.service.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../models/product.model';
 import { Observable } from 'rxjs';
-import { HttpClient } from "@angular/common/http";
-import { map } from "rxjs/operators";
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ProductService{
+export class ProductService {
 
   private products: Observable<Product[]>;
 
@@ -16,7 +16,7 @@ export class ProductService{
 
 
   public all(): Observable<Product[]> {
-    return this.http.get("./assets/products.json")
+    return this.http.get('./assets/products.json')
       .pipe(map((response: any) => response)
       );
   }
